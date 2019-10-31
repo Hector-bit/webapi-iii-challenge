7f0d2c41dc8827c552b06649b71427f27ea92874
Hector-bit <hectormartinez.2978@gmail.com>
2019-10-30 23:06:09 -0700

    CRUD operations runnging

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 const express = require('express');
-
+const helmet = require('helmet');
+const postRouter = require('./posts/postRouter');
 const server = express();
+
+function logger(req, res, next) {
+  console.log(`${req.method} to ${req.path} from ${req.url} ${Date.now() / 1000}}`)
+};
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
 });
 
 //custom middleware
-
-function logger(req, res, next) {
-
-};
+server.use('/api/posts', postRouter);
+server.use(logger);
+server.use(helmet());
+server.use(express.json());
 
 module.exports = server;
