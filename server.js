ef059184c548ff20bb2c32622f869eb82c163468
Hector-bit <hectormartinez.2978@gmail.com>
2019-10-31 02:38:03 -0700

    Hector-Martinez

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const helmet = require('helmet');
 const postRouter = require('./posts/postRouter');
+const userRouter = require('./users/userRouter');
 const server = express();
 
 function logger(req, res, next) {
   console.log(`${req.method} to ${req.path} from ${req.url} ${Date.now() / 1000}}`)
+
+  next();
 };
 
 server.get('/', (req, res) => {
@@ -13,6 +16,7 @@ server.get('/', (req, res) => {
 
 //custom middleware
 server.use('/api/posts', postRouter);
+server.use('/api/users', userRouter);
 server.use(logger);
 server.use(helmet());
 server.use(express.json());
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,47 +1,107 @@
 const express = require('express');
-
+const userdb = require('./userDb');
 const router = express.Router();
 
-router.post('/', (req, res) => {
+//custom middleware
 
+function validateUserId(req, res, next) {
+    if (!req.body.id) {
+        res.status(400).json({ message: "missing user" });
+      } 
+      next();
+};
+
+function validateUser(req, res, next) {
+    console.log("validateUser", req.body);
+    if (!req.body) {
+      res.status(400).json({ message: "missing user data" });
+    } else if (!req.body.name) {
+      res.status(400).json({ message: "missing required name field" });
+    }
+    next();
+  }
+
+function validatePost(req, res, next) {
+    if (!req.body) {
+        res.status(400).json({ message: "missing post data" });
+      } else if (!req.body.name) {
+        res.status(400).json({ message: "missing required text field" });
+      }
+      next();
+};
+
+router.post('/', validateUser, (req, res) => {
+    // console.log("router.post", req.body);
+
+    // const user = req.body;
+
+    userdb.insert(req.body)
+    .then(user => {
+        res.status(201).json(user)
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({ message: 'Error adding the user'} )
+    })
 });
 
-router.post('/:id/posts', (req, res) => {
-
+router.post('/:id/posts', validatePost, (req, res) => {
+    userdb.insert(req.body)
+    .then(newPost => {
+        res.status(200).json(newPost)
+    })
+    .catch(err => {
+        console.log("error", err)
+        res.status(500).json({ error: "the users post was not added"})
+    })
 });
 
 router.get('/', (req, res) => {
-
+    userdb.get(req.body)
+    .then(user => {
+        if (user) {
+          res.status(200).json(user);
+        } else {
+          res.status(404).json({ message: 'post not found' });
+        }
+      })
 });
 
 router.get('/:id', (req, res) => {
-
+    userdb.getById(req.params.id)
+    .then(user => {
+        if (user) {
+          res.status(200).json(user);
+        } else {
+          res.status(404).json({ message: 'post not found' });
+        }
+      })
 });
 
 router.get('/:id/posts', (req, res) => {
-
+    userdb.getUserPosts(req.params.id)
+    .then(user => {
+        if (user) {
+          res.status(200).json(user);
+        } else {
+          res.status(404).json({ message: 'post not found' });
+        }
+      })
 });
 
 router.delete('/:id', (req, res) => {
-
+    userdb.remove(req.params.id)
+    .then(user => {
+        if (user) {
+          res.status(200).json('user was deleted');
+        } else {
+          res.status(404).json({ message: 'user not found' });
+        }
+      })
 });
 
 router.put('/:id', (req, res) => {
 
 });
-
-//custom middleware
-
-function validateUserId(req, res, next) {
-
-};
-
-function validateUser(req, res, next) {
-
-};
-
-function validatePost(req, res, next) {
-
-};
 
 module.exports = router;
